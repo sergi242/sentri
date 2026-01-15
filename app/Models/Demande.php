9ee76f6dda765daaa9fd8237e9d5a08fb8f4eee5
jeremie-security <mijemiyouna@gmail.com>
2026-01-15 13:38:08 +0100

    Ajout de la prise en charge de la suppression douce dans le modèle Demande

--- app/Models/Demande.php
+++ app/Models/Demande.php
@@ -8,10 +8,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Demande extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $guarded = [];
 
     /**
